551a0a0e61df05da8808393e70acee5e8c010ef2
gayoung <rkdud91455@gmail.com>
2023-05-14 20:54:38 +0900

    ejs 설정

---
+++ test/js/script.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+// EJS를 사용하기 위한 설정
+app.set('view engine', 'ejs');
+
+// EJS 파일이 위치한 디렉토리 설정 (옵션)
+app.set('views', '../views');
+
+// 기타 Express 설정과 미들웨어 등을 추가합니다.
+
+// 라우트 설정
+app.get('/', (req, res) => {
+  // index.ejs 파일을 렌더링하여 응답합니다.
+  res.render('index');
+});
+
+// 서버 시작
+app.listen(3000, () => {
+  console.log('서버가 3000번 포트에서 실행 중입니다.');
+});
