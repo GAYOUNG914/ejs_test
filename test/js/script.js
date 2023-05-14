27fd646a381aa7807f8ea31123faa499191b4b2d
gayoung <rkdud91455@gmail.com>
2023-05-15 01:40:54 +0900

    third

--- test/js/script.js
+++ test/js/script.js
@@ -9,10 +9,24 @@ app.set('views', '../views');
 
 // 기타 Express 설정과 미들웨어 등을 추가합니다.
 
+app.use(express.static('public'));
+
+
+let users = [
+  {
+    name:'firstMan',
+    email:"firstEmail"
+  },
+  {
+    name:'secondMan',
+    email:"secondEmail"
+  }
+]
+
 // 라우트 설정
 app.get('/', (req, res) => {
   // index.ejs 파일을 렌더링하여 응답합니다.
-  res.render('index');
+  res.render('index', { users });
 });
 
 // 서버 시작
